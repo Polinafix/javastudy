82b001bd397cfaeed301d592a13d47bf67ce1743
Polinafix <pd.fiksson@gmail.com>
2015-05-02 21:45:11 +0300

    override and overload methods

--- Tasks/src/PointChild.java
+++ Tasks/src/PointChild.java
@@ -15,7 +15,54 @@ public class PointChild extends Point {
 
     @Override
     public String quarter() {
-        return "override from child";
+        return "override from child. " + super.quarter(); // вызов метода из родительского класса
     }
+
+    public String quarter(String msg) {
+        return "String... msg: " + msg;
+    }
+
+    public String quarter(String... msg) {
+        return "String... msg: " + msg;
+    }
+
+    public String quarter(Integer msg) {
+        return "Integer msg";
+    }
+
+    public String quarter(Integer... msg) {
+        String result = "Integer... msg: len:" + msg.length;
+        for (Integer num : msg) {
+            result += "; " + num;
+        }
+        return result;
+    }
+
+    public String quarter(String s, Integer... msg) {
+        return "String s, Integer... msg";
+    }
+
+    public static void main(String[] args) {
+        PointChild p = new PointChild(1, 2);
+        System.out.println(p.quarter());
+        System.out.println(p.quarter("string"));
+        System.out.println(p.quarter("string", "string"));
+        System.out.println(p.quarter(12));
+        System.out.println(p.quarter(12, 14));
+        System.out.println(p.quarter(12, 14, 16, 17));
+        System.out.println(p.quarter("string", 12, 14, 16, 17));
+
+        Point a = new Point(0, 0);
+        System.out.println(a.quarter());
+    }
+
+    // invalid overloading
+/*    public String quarter(String... s, Integer... msg) {
+        return "Integer... msg";
+    }
+
+    public String quarter(String... s, Integer msg) {
+        return "Integer... msg";
+    }*/
 
 }
